5307f9c466b88bc60114a02fd181e6b3c6cae61e
SamirKurbatov <samir.kurbatov.03@inbox.ru>
2023-11-03 22:03:16 +0300

    AddModels

---
+++ PasswordGeneratorApp.WPF/Models/Password.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGeneratorApp.WPF.Models
+{
+    public class Password
+    {
+        public string Generated { get; set; }
+
+        public int Length { get; set; }
+    }
+}
