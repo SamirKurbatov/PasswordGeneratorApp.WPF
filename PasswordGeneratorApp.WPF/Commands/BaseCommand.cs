3b36a692b441b5aa7ba499b77215e7f3fca65b3d
SamirKurbatov <samir.kurbatov.03@inbox.ru>
2023-11-03 22:03:09 +0300

    AddCommands

---
+++ PasswordGeneratorApp.WPF/Commands/BaseCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace PasswordGeneratorApp.WPF.Commands
+{
+    public abstract class BaseCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public virtual bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public abstract void Execute(object? parameter);
+
+        public virtual void OnCanExecutedChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ PasswordGeneratorApp.WPF/Commands/GenerateCommand.cs
@@ -0,0 +1,33 @@
+﻿using PasswordGeneratorApp.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PasswordGeneratorApp.WPF.Commands
+{
+    public class GenerateCommand : BaseCommand
+    {
+        private readonly PasswordViewModel passwordViewModel;
+
+        public GenerateCommand(PasswordViewModel passwordViewModel)
+        {
+            this.passwordViewModel = passwordViewModel;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            var random = new Random();
+
+            for (int i = 1; i < passwordViewModel.PasswordLength; i++)
+            {
+                var randomChar = (char)('a' + random.Next(0, 26));
+                int randomNumber = random.Next(0, 20);
+
+                passwordViewModel.GeneratedPassword += $"{randomChar}{randomNumber}";
+            }
+        }
+    }
+}
