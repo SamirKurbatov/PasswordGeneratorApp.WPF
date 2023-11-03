8cf0cb2c5748a229dc79e982b36a06d867db3eb7
SamirKurbatov <samir.kurbatov.03@inbox.ru>
2023-11-03 22:03:01 +0300

    AddViewModel

---
+++ PasswordGeneratorApp.WPF/ViewModels/BaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PasswordGeneratorApp.WPF.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnProperetyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ PasswordGeneratorApp.WPF/ViewModels/PasswordViewModel.cs
@@ -0,0 +1,43 @@
+﻿using PasswordGeneratorApp.WPF.Commands;
+using PasswordGeneratorApp.WPF.Models;
+using System.Windows.Input;
+
+namespace PasswordGeneratorApp.WPF.ViewModels
+{
+    public class PasswordViewModel : BaseViewModel
+    {
+        private Password password;
+
+        public int PasswordLength
+        {
+            get => password.Length;
+            set
+            {
+                password.Length = value;
+                OnProperetyChanged();
+            }
+        }
+
+        public string GeneratedPassword
+        {
+            get => password.Generated;
+            set
+            {
+                password.Generated = value;
+                OnProperetyChanged();
+            }
+        }
+
+        public bool IsUpperLetter { get; set; }
+
+        public bool IsLowerLetter { get; set; }
+
+        public ICommand GenerateCommand { get; }
+
+        public PasswordViewModel()
+        {
+            password = new();
+            GenerateCommand = new GenerateCommand(this);
+        }
+    }
+}
